f186144b7e7b5dfa87a7e87c1d3cf3a9dd494cac
Caio Lima <caiolimapires61@gmail.com>
2023-12-20 11:43:51 -0300

    feat: Cofiguração inicial do projeto

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import BackgroundCofe from "@layout/atoms/BackgroundCofe"
+import '../src/styles/app.css'
+function App() {
+  return (
+    <div className="app">
+      <BackgroundCofe/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/layout/components/atoms/BackgroundCofe/index.tsx
@@ -0,0 +1,7 @@
+import "../../../styles/BackgroundCofe.css"
+
+const BackgroundCofe=()=>{
+    return(<div className="background"></div>)   
+}
+
+export default BackgroundCofe;
