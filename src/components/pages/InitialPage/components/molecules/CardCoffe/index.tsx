dd72b11d0680a9a1f35a5c2de46576e37badc976
Caio Lima <caiolimapires61@gmail.com>
2024-01-04 21:56:30 -0300

    feat: criado contexto global usando zustand e alterada estilizações

--- src/components/pages/InitialPage/components/molecules/CardCoffe/index.tsx
+++ src/components/pages/InitialPage/components/molecules/CardCoffe/index.tsx
@@ -1,6 +1,7 @@
 import Card from '@ui/atoms/Card';
 import './style.css';
 import DetailsCoffe from '../DetailsCoffe';
+import { useAlterStateButton } from '../../../../../context/ButtonChangeTheViewType/context';
 
 type Props = {
   data: CoffeDetailsProps[];
@@ -15,19 +16,37 @@ export type CoffeDetailsProps = {
   popular: boolean;
   available: boolean;
 };
+
 const CardCoffe = ({ data }: Props) => {
-  return data.map((item) => (
-    <div className="containerCardCoffe" key={item.id}>
-      <Card image={item.image} />
-      <DetailsCoffe
-        price={item.price}
-        rating={item.rating}
-        text={item.name}
-        votes={item.votes}
-        avilable={item.available}
-      />
-    </div>
-  ));
+  const [buttonState] = useAlterStateButton((state) => [state.buttomState]);
+
+  return data.map((item) =>
+    buttonState === 'AvailableNow' ? (
+      item.available ? (
+        <div className="containerCardCoffe" key={item.id}>
+          <Card image={item.image} />
+          <DetailsCoffe
+            price={item.price}
+            rating={item.rating}
+            text={item.name}
+            votes={item.votes}
+            avilable={item.available}
+          />
+        </div>
+      ) : null
+    ) : (
+      <div className="containerCardCoffe" key={item.id}>
+        <Card image={item.image} />
+        <DetailsCoffe
+          price={item.price}
+          rating={item.rating}
+          text={item.name}
+          votes={item.votes}
+          avilable={item.available}
+        />
+      </div>
+    )
+  );
 };
 
 export default CardCoffe;
